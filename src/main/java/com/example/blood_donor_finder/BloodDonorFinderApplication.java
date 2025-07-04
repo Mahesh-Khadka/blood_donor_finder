03b99cb6ea9d0af1bcfab34ee3b6f8b061ef1e09
Mahesh-Khadka <maheshkhadka438@gmail.com>
2025-07-04 23:53:24 +0545

    Initial commit

---
+++ src/main/java/com/example/blood_donor_finder/BloodDonorFinderApplication.java
@@ -0,0 +1,13 @@
+package com.example.blood_donor_finder;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BloodDonorFinderApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(BloodDonorFinderApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/blood_donor_finder/BloodDonorFinderApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.blood_donor_finder;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class BloodDonorFinderApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
