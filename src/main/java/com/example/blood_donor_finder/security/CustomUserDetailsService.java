7688f5e886e7a32eb99187c819eed0a9d29574ae
Mahesh-Khadka <maheshkhadka438@gmail.com>
2025-07-06 01:20:01 +0545

    Add CustomUserDetailsService to fetch users from database.

---
+++ src/main/java/com/example/blood_donor_finder/security/CustomUserDetailsService.java
@@ -0,0 +1,32 @@
+package com.example.blood_donor_finder.security;
+
+import com.example.blood_donor_finder.entity.User;
+import com.example.blood_donor_finder.repository.UserRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.authority.SimpleGrantedAuthority;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+import org.springframework.stereotype.Service;
+
+import java.util.Collections;
+
+@Service
+public class CustomUserDetailsService implements UserDetailsService {
+    private final  UserRepository userRepository;
+
+    @Autowired
+    public CustomUserDetailsService (UserRepository userRepository){
+        this.userRepository = userRepository;
+    }
+    @Override
+    public UserDetails loadUserByUsername(String email) throws UsernameNotFoundException{
+        User user = userRepository.findByEmail(email).orElseThrow(()->new RuntimeException("Email not found."));
+        return new org.springframework.security.core.userdetails.User(
+                user.getEmail(),
+                user.getPassword(),
+                Collections.singletonList(new SimpleGrantedAuthority(user.getRole()))
+
+        );
+    }
+}
